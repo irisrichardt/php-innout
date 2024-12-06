fd9cdacc9a9b14b6dd8ed382a7d47d8e666c4379
Íris <iris.balk04@gmail.com>
2024-12-06 17:26:09 -0300

    Protegendo rotas

--- src/controllers/imobiliarias.php
+++ src/controllers/imobiliarias.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-requireValidSession();
+requireValidSession(false, false, true);
 
 loadModel("Imobiliaria");
 
--- src/controllers/locatarios.php
+++ src/controllers/locatarios.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-requireValidSession();
+requireValidSession(false, true, false);
 
 loadModel("Locatario");
 
